007fda69222f87e80a50ea358be864817b47ff25
Haikal Fikri <haikaljr1245@gmail.com>
2025-03-22 09:55:43 +0700

    drf-spectacular setup

--- drf-course-api/drf_course/settings.py
+++ drf-course-api/drf_course/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'silk',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -122,5 +123,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'DRF Course API',
+    'DESCRIPTION': 'API for DRF Course',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # Other Settings
 }
